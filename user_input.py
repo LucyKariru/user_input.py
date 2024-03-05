daad807a41768ad9e47f49b7b756a875c5da4fa6
LucyKariru <162087554+LucyKariru@users.noreply.github.com>
2024-03-05 12:47:15 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = "Lucy Kariru N."
+firstName = input("Enter your first name: ")
+print("My name is: " +name +"My friends' name is: " +firstName)
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+print("Hello: " +name.upper() + "\nyou are: " +age + "\nyears old and live in: " +location)
